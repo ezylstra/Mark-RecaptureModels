ec1613b4a4f59184f0fa320cc3995043869d525f
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2021-11-16 08:58:12 -0700

    Upload hummingbird data

---
+++ scripts/vetting_data_2021.R
@@ -0,0 +1,13 @@
+# Vetting HMN's 2021 banding data 
+# Gaby Samaniego gabysamaniego@email.arizona.edu 
+# 2021-11-15 
+
+# Set working directory
+setwd("C:/Users/My Elite/Documents/R/HummingBird/data")
+
+# Bring in data
+data <- read.csv("HC_SWRS1_HMNBandingData_2021_GS.csv")
+
+datahsr <- read.csv("HSR_ALL_HMNBandingData_2021.csv")
+
+

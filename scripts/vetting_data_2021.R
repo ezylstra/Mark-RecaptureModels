bfcc592501ee0d1ac8d2161b84c8bb4b501f5a1f
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2021-11-22 15:21:27 -0700

    Comments

--- scripts/vetting_data_2021.R
+++ scripts/vetting_data_2021.R
@@ -11,11 +11,11 @@ setwd("C:/Users/My Elite/Documents/R/HummingBird/data")
 data <- read.csv("HC_SWRS1_HMNBandingData_2021_GS.csv")
 head(data)
 
-example <- read.csv("example.csv")
+example <- read.csv("example.csv") # It has lower case in some columns 
 head(example)
 
 # Change all characters to uppercase
-uppercase_data <- data.frame(lapply(data, function(variables) {
+uppercase_data <- data.frame(lapply(data, function(variables) { # from the internet... 
                                 if (is.character(variables)) {
                                   return(toupper(variables))
                                 } else {
@@ -27,4 +27,5 @@ head(uppercase_data)
 
 uppercase_data_2 <-mutate_all(example, .funs=toupper) # all allows you to perform 
                                             # an operation on all variables at once 
+                                            # from Kira's script 
 head(uppercase_data_2)

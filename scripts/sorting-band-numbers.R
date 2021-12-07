d5d79854af10d03009cb4663eff0034575f1e46a
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2021-12-07 10:19:52 -0700

    New script to work sorting band numbers

---
+++ scripts/sorting-band-numbers.R
@@ -0,0 +1,17 @@
+# Sorting band numbers
+# Gaby Samaniego
+
+install.packages("tidyverse")
+library(tidyverse)
+
+# Set working directory
+getwd()
+setwd("C:/Users/gabym/Documents/R/HummingBird/data")
+
+# Bring in data
+data <- read.csv("2021-fh-alldata-1.csv")
+head(data1)
+
+# Sort data by band number
+
+

c84b71862e5a8a4b2be1838c2ae7a974414f989e
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2021-12-07 11:06:44 -0700

    Sort band numbers alphabetically

--- scripts/sorting-band-numbers.R
+++ scripts/sorting-band-numbers.R
@@ -9,9 +9,15 @@ getwd()
 setwd("C:/Users/gabym/Documents/R/HummingBird/data")
 
 # Bring in data
-data <- read.csv("2021-fh-alldata-1.csv")
-head(data1)
+data <- read.csv("2021-fh-alldata.csv")
+head(data)
 
-# Sort data by band number
+# Sort band numbers alphabetically 
+data[order(data$Band.Number),] 
+
+# Unique values should be band status 1, duplicate values should be band status R
+duplicated(data$Band.Number)
+
+
 
 

e21341af7d91f1e1abd819df34ddbe73e5bc2abe
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2022-01-31 10:37:10 -0700

    Used a dataset with mistakes to verify code

--- scripts/vetting_data_2021.R
+++ scripts/vetting_data_2021.R
@@ -2,35 +2,53 @@
 # Gaby Samaniego gabysamaniego@email.arizona.edu 
 # 2021-11-15 
 
-library(tidyverse)
+install.packages("qdap")
+
+library(tidyverse) # for everything
+library(qdap)      # to remove extra spaces 
 
 # Set working directory
 setwd("C:/Users/gabym/Documents/R/HummingBird/data")
 
 # Bring in data
-data <- read.csv("HC_SWRS1_HMNBandingData_2021_GS.csv")
-head(data)
-
-example <- read.csv("example.csv") # It has lower case in some columns 
-head(example)
+example <- read.csv("example.csv") # It has lower case in some columns and other
+head(example)                      # errors to try the code
 
 # Change all characters in the database to uppercase
-uppercase_data <- data.frame(lapply(data, function(variables) { # from the internet... 
-                                if (is.character(variables)) {
-                                  return(toupper(variables))
+
+uppercase_data <- data.frame(lapply(example, function(variables) { # from the internet 
+                                if (is.character(variables)) {     # notes in notebook
+                                  return(toupper(variables))  
                                 } else {
                                   return(variables)}
                               }),
                        stringsAsFactors = FALSE) 
+
 head(uppercase_data) # it worked! 
 
 
-uppercase_data_2 <-mutate_all(example, .funs=toupper) # all allows you to perform 
-                                            # an operation on all variables at once 
-                                            # from Kira's script, also worked  
-head(uppercase_data_2)
+head(example)
+uppercase_data_2 <-mutate_all(example, .funs=toupper) # from Kira's script  
+head(uppercase_data_2) # also worked!                 # Notes in notebook
+
+# Remove multiple spaces in rows 
+trimws(example,"both")
+head(example)
+
+
+# Check each column to find mistakes
+
+
+
+# Check the sequence of the band numbers 
+
+# Create csv with clean data for each banding session 
+
 
 # Find duplicates in Band_number 
+
+
+# Separate combined columns 
 
 
 

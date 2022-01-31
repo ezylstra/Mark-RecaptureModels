feee3154665b50ddc27c4ef47fc8d5a224730711
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2022-01-31 13:43:46 -0700

    Tried to remove white spaces

--- scripts/vetting_data_2021.R
+++ scripts/vetting_data_2021.R
@@ -17,6 +17,7 @@ head(example)                      # errors to try the code
 
 # Change all characters in the database to uppercase
 
+# First try:
 uppercase_data <- data.frame(lapply(example, function(variables) { # from the internet 
                                 if (is.character(variables)) {     # notes in notebook
                                   return(toupper(variables))  
@@ -27,14 +28,23 @@ uppercase_data <- data.frame(lapply(example, function(variables) { # from the in
 
 head(uppercase_data) # it worked! 
 
-
-head(example)
-
-uppercase_data_2 <-mutate_all(example, .funs=toupper) # from Kira's script  
+# Second try:
+uppercase_data_2 <- mutate_all(example, .funs=toupper) # from Kira's script  
 head(uppercase_data_2) # also worked!                 # Notes in notebook
 
 # Remove all leading and trailing white spaces
-banding$Initials.Bdr <- str_trim(banding$Initials.Bdr, side=c("both"))
+banding$Initials.Bdr <- str_trim(banding$Initials.Bdr, side=c("both")) # from Kira's script
+
+remove_spaces <- mutate_all(example, .funs=str_trim(example, side = "both"))
+
+remove_spaces_2 <- data.frame(lapply(example, function(varaibles){
+                              if (str_trim(example, side = "both")) {
+                              } else {
+                                return(varaibles)}
+                              }),
+                              stringsAsFactors = FALSE)
+
+
    # can I use mutate_all for this purpose? I tried and failed :( 
 
 # Check each column to find mistakes  

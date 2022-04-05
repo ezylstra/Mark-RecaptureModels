89e43f3d3a208a1bb2626df9acfd0277bb370d28
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2022-04-05 12:05:33 -0700

    Add NA values to validation code

--- scripts/ML-data-quality-reporting.R
+++ scripts/ML-data-quality-reporting.R
@@ -30,15 +30,6 @@ ML_data <- ML_data %>%
 head(ML_data)
 class(ML_data$Date)
 
-# Change Time column from character to posix 
-class(ML_data$Time)
-
-ML_data <- ML_data %>% 
-           mutate(Time = hm(Time))
-
-head(ML_data)
-class(ML_data$Time)
-
 # Split date by year, month, and day
 ML_data <- mutate(ML_data, Year = year(Date), Month = month(Date),Day = day(Date))  
 head(ML_data)
@@ -62,40 +53,36 @@ ML <-
     label = "ML Data Validation",
     actions = al) %>% 
   col_is_date(vars(Date)) %>% 
-  col_is_posix(vars(Time)) %>% 
   col_vals_equal(vars(Bander), value = "GS") %>%    
   col_vals_equal(vars(Location), value = "ML") %>%  
   col_vals_in_set(vars(Species), set = c("ANHU","ALHU","BBLH","BCHU","BADE",
                                          "BEHU","BTMG","BTLH","BUFH","BALO",
                                          "CAHU","COHU","LUHU","RIHU","HYHU",
                                          "RTHU","RUHU","VCHU","WEHU","UNHU")) %>% 
-  col_vals_in_set(vars(Sex), set = c("M","F","U")) %>% 
-  col_vals_in_set(vars(Age), set = c("0","1","2","5")) %>% 
-  col_vals_in_set(vars(Band.Status), set = c("1","R","F","4","5","6","8")) %>%  
+  col_vals_in_set(vars(Sex), set = c("M","F","U",NA)) %>% 
+  col_vals_in_set(vars(Age), set = c("0","1","2","5",NA)) %>% 
+  col_vals_in_set(vars(Band.Status), set = c("1","R","F","4","5","6","8",NA)) %>%  
   col_vals_in_set(vars(Tarsus.Measurement), set = c("B","C","D","E","F","G","H",
                                                     "I","J","K","L","M","N","O",NA)) %>% 
   col_vals_in_set(vars(Band.Size), set = c("B","C","D","E","F","G","H","I","J",
-                                           "K","L","M","N","O")) %>% 
-  col_vals_regex(vars(Band.Number), regex = pattern) %>% 
-  col_vals_in_set(vars(Leg.Condition), set = c("1","2","3","4","5","6","7")) %>% 
-  col_vals_regex(vars(Replaced.Band.Number), regex = pattern) %>%
+                                           "K","L","M","N","O",NA)) %>% 
+  col_vals_in_set(vars(Leg.Condition), set = c("1","2","3","4","5","6","7",NA)) %>% 
   col_vals_in_set(vars(Gorget.Color), set = c("O","R","V","P","B","G","GP","NS",
-                                              "LS","MS","HS")) %>% 
+                                              "LS","MS","HS",NA)) %>% 
   col_vals_between(vars(Gorget.Count),0, 99, na_pass = TRUE) %>% 
   col_vals_between(vars(Head.Count),0, 99, na_pass = TRUE) %>% 
-  col_vals_in_set(vars(Grooves), set = c("0","1","2","3")) %>% 
-  col_vals_in_set(vars(Buffy), set = c("Y","N","S")) %>% 
+  col_vals_in_set(vars(Grooves), set = c("0","1","2","3",NA)) %>% 
+  col_vals_in_set(vars(Buffy), set = c("Y","N","S",NA)) %>% 
   col_vals_between(vars(Wing.Chord), 35, 80, na_pass = TRUE) %>% 
   col_vals_between(vars(Culmen), 12, 30, na_pass = TRUE) %>% 
-  col_vals_in_set(vars(Fat), set = c("0","1","2","3","P","T")) %>%
-  col_vals_in_set(vars(CP.Breed), set = c("9","8","7","5","2")) %>%
-  col_vals_in_set(vars(Head.Gorget), set = c("1","2","3","F","L","M","R")) %>%
-  col_vals_in_set(vars(Body), set = c("1","2","3","F","L","M","R")) %>%
+  col_vals_in_set(vars(Fat), set = c("0","1","2","3","P","T",NA)) %>%
+  col_vals_in_set(vars(CP.Breed), set = c("9","8","7","5","2",NA)) %>%
+  col_vals_in_set(vars(Head.Gorget), set = c("1","2","3","F","L","M","R",NA)) %>%
+  col_vals_in_set(vars(Body), set = c("1","2","3","F","L","M","R",NA)) %>%
   col_vals_in_set(vars(Primaries), set = c("1","2","3","4","5","6","7","8","9",
-                                           "0","F","L","M","R")) %>%
-  col_vals_in_set(vars(Secondaries), set = c("1","2","3","4","5","6","F","L","M","R")) %>%
-  col_vals_in_set(vars(Tail.1), set = c("1","2","3","4","5","F","L","M","R")) %>% 
-  col_vals_between(vars(Weight), 2, 12, na_pass = TRUE)
+                                           "0","F","L","M","R",NA)) %>%
+  col_vals_in_set(vars(Secondaries), set = c("1","2","3","4","5","6","F","L","M","R",NA)) %>%
+  col_vals_in_set(vars(Tail.1), set = c("1","2","3","4","5","F","L","M","R",NA))
   
 interrogate(ML)
 

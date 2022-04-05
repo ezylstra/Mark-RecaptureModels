c209aa26a8df98b59900b187384b55c85560c270
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2022-04-05 11:10:38 -0700

    Fix NA problem!!! Thanks Jeff!!!

--- scripts/ML-data-quality-reporting.R
+++ scripts/ML-data-quality-reporting.R
@@ -11,55 +11,94 @@ setwd("C:/Users/gabym/Documents/R/HummingBird/data")
 
 # Bring in data 
 ML_data <- read.csv("ML_data.csv", na.strings = c("","NA"))
+head(ML_data)
 
 # Capitalize all characters and factors
-ML_data <-mutate_all(ML_data, .funs=toupper)
+ML_data <- mutate_all(ML_data, .funs=toupper)
+head(ML_data)
 
 # Create columns for CMR and Protocol
 ML_data <- mutate(ML_data, CMR = "Y", Protocol = "HMN")
+head(ML_data)
 
-# Split date by year, month, and day, now is not working!
-ML_data <- mutate(ML_data, Year = year(Date), Month = month(Date),Day = day(Date))  # old, now is not working!!! 
+# Change Date column from character to date
+class(ML_data$Date)
 
-# Bring in KoBo datasheet structure to verify column names
-KoBo_datasheet_structure <- read.csv("KoBo-datasheet.csv")
-colnames(KoBo_datasheet_structure)
+ML_data <- ML_data %>% 
+           mutate(Date = mdy(Date))
+
+head(ML_data)
+class(ML_data$Date)
+
+# Change Time column from character to posix 
+class(ML_data$Time)
+
+ML_data <- ML_data %>% 
+           mutate(Time = hm(Time))
+
+head(ML_data)
+class(ML_data$Time)
+
+# Split date by year, month, and day
+ML_data <- mutate(ML_data, Year = year(Date), Month = month(Date),Day = day(Date))  
+head(ML_data)
+class(ML_data$Month)
 
 # Data quality reporting for Mount Lemmon site (ML)
 
 # Set thresholds for report 
-al <- action_levels(warn_at = 0.1, stop_at = 0.1) 
+al <- action_levels(warn_at = 1, stop_at = 1) 
 
-head(ML_data)
+# Create pattern (regex) to check band numbers 
+pattern <- "[a-z]{1}[0-9]{5}"
+ML_data %>% test_col_vals_regex(vars(Band.Number), regex = pattern)
+
+colnames(ML_data)
 
 ML <- 
   create_agent(
-    tbl = new,
+    tbl = ML_data,
     tbl_name = "ML_data",
     label = "ML Data Validation",
     actions = al) %>% 
-  col_vals_equal(vars(Bander), value = "GS") %>%   # 1
-  col_vals_equal(vars(Location), value = "ML") %>% # 2
+  col_is_date(vars(Date)) %>% 
+  col_is_posix(vars(Time)) %>% 
+  col_vals_equal(vars(Bander), value = "GS") %>%    
+  col_vals_equal(vars(Location), value = "ML") %>%  
   col_vals_in_set(vars(Species), set = c("ANHU","ALHU","BBLH","BCHU","BADE",
                                          "BEHU","BTMG","BTLH","BUFH","BALO",
                                          "CAHU","COHU","LUHU","RIHU","HYHU",
-                                         "RTHU","RUHU","VCHU","WEHU","UNHU")) %>% # 3
-  col_vals_in_set(vars(Sex), set = c("M","F","U")) %>% # 4
-  col_vals_in_set(vars(Age), set = c("0","1","2","5")) %>% # 5
-  col_vals_in_set(vars(Band.Status), set = c("1","R","F","4","5","6","8")) %>% # 6 
+                                         "RTHU","RUHU","VCHU","WEHU","UNHU")) %>% 
+  col_vals_in_set(vars(Sex), set = c("M","F","U")) %>% 
+  col_vals_in_set(vars(Age), set = c("0","1","2","5")) %>% 
+  col_vals_in_set(vars(Band.Status), set = c("1","R","F","4","5","6","8")) %>%  
   col_vals_in_set(vars(Tarsus.Measurement), set = c("B","C","D","E","F","G","H",
-                                                    "I","J","K","L","M","N","O")) %>% # 7
+                                                    "I","J","K","L","M","N","O",NA)) %>% 
   col_vals_in_set(vars(Band.Size), set = c("B","C","D","E","F","G","H","I","J",
-                                           "K","L","M","N","O")) %>% # 8
-  col_vals_in_set(vars(Leg.Condition), set = c("1","2","3","4","5","6","7")) %>% # 9
+                                           "K","L","M","N","O")) %>% 
+  col_vals_regex(vars(Band.Number), regex = pattern) %>% 
+  col_vals_in_set(vars(Leg.Condition), set = c("1","2","3","4","5","6","7")) %>% 
+  col_vals_regex(vars(Replaced.Band.Number), regex = pattern) %>%
   col_vals_in_set(vars(Gorget.Color), set = c("O","R","V","P","B","G","GP","NS",
-                                              "LS","MS","HS")) %>% # 10
-  col_vals_between(vars(Gorget.Count),0, 99, na_pass = TRUE) %>% # 11
-  col_vals_between(vars(Head.Count),0, 99, na_pass = TRUE) %>% # 12
-  col_vals_in_set(vars(Grooves), set = c("0","1","2","3")) %>% # 13
-  col_vals_in_set(vars(Buffy), set = c("Y","N","S")) # 14 
-
+                                              "LS","MS","HS")) %>% 
+  col_vals_between(vars(Gorget.Count),0, 99, na_pass = TRUE) %>% 
+  col_vals_between(vars(Head.Count),0, 99, na_pass = TRUE) %>% 
+  col_vals_in_set(vars(Grooves), set = c("0","1","2","3")) %>% 
+  col_vals_in_set(vars(Buffy), set = c("Y","N","S")) %>% 
+  col_vals_between(vars(Wing.Chord), 35, 80, na_pass = TRUE) %>% 
+  col_vals_between(vars(Culmen), 12, 30, na_pass = TRUE) %>% 
+  col_vals_in_set(vars(Fat), set = c("0","1","2","3","P","T")) %>%
+  col_vals_in_set(vars(CP.Breed), set = c("9","8","7","5","2")) %>%
+  col_vals_in_set(vars(Head.Gorget), set = c("1","2","3","F","L","M","R")) %>%
+  col_vals_in_set(vars(Body), set = c("1","2","3","F","L","M","R")) %>%
+  col_vals_in_set(vars(Primaries), set = c("1","2","3","4","5","6","7","8","9",
+                                           "0","F","L","M","R")) %>%
+  col_vals_in_set(vars(Secondaries), set = c("1","2","3","4","5","6","F","L","M","R")) %>%
+  col_vals_in_set(vars(Tail.1), set = c("1","2","3","4","5","F","L","M","R")) %>% 
+  col_vals_between(vars(Weight), 2, 12, na_pass = TRUE)
+  
 interrogate(ML)
+
 
 # to get errors in the rows: Michael suggestions 
 
@@ -83,6 +122,7 @@ unique(ML_data$Gorget.Color)
 unique(ML_data$Gorget.Count)
 unique(ML_data$Head.Count)
 unique(ML_data$Grooves)
+unique(ML_data$Body)
 
 # Find duplicated band numbers
 any(duplicated(ML_data$Band.Number)) # Are there duplicates? true or false

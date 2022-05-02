9dce6f1f81ba7af259bff2e8780fa888f10e05e9
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2022-05-02 12:03:44 -0700

    Use code to vet PA data, update it to use the code with PA data

--- scripts/ML-data-quality-reporting.R
+++ scripts/ML-data-quality-reporting.R
@@ -6,24 +6,38 @@
 library(tidyverse)
 library(pointblank)
 library(lubridate)
+library(stringr)
 
 ##### Data wrangling ##### 
 
 # Bring in data 
-data <- read.csv("data/HC_0421_HMNBandingData_2022.csv", 
+data <- read.csv("data/PA_0410_HMNBandingData_2022.csv", 
                     na.strings = c("","NA"))
 
-# If Molt information is all F, R will treat it as a logical vector and replace 
-# it with FALSE, so we need to change that by telling R to treat that as a 
-# character 
+# If Molt information is all F (fresh), R will treat it as a logical vector and 
+# replace it with FALSE, so we need to change that by creating a new character 
+# column 
+
+class(data$Secondaries)
+is.logical(data$Secondaries)
+
+data <- data %>% 
+  mutate(sec_2 = if_else(Secondaries == FALSE, "F"))
+
+         
+         
+if(is.logical(data$Secondaries)){
+  data <- data %>% 
+    mutate(sec2 = if_else(Secondaries == FALSE, "F"))
+}
+
 
 if(is.logical(data$Secondaries)){
   data <- data %>% 
-    mutate(sec_2 = if_else(Secondaries == FALSE, F, NA)) %>% 
-    mutate(Secondaries = sec_2) %>% 
-    select(-sec_2)
+    mutate(sec2 = if_else(Secondaries == FALSE, "F")) %>% 
+    mutate(Secondaries = sec2) %>% 
+    select(-sec2)
 }
-
 
 # try this and is not working 
 
@@ -33,6 +47,9 @@ data <- data %>%
 # Capitalize all characters and factors across data frame 
 data <- mutate_all(data, .funs=toupper)
 
+# Remove all leading and trailing white spaces
+data <- mutate_all(data,str_trim, side=c("both"))
+
 # Create columns to indicate if the data will be used for capture mark recapture
 # (CMR) analysis and to add the protocol used to collect it
 data <- mutate(data, CMR = "Y", Protocol = "HMN")
@@ -40,7 +57,7 @@ data <- mutate(data, CMR = "Y", Protocol = "HMN")
 # Change Date column from character to date
 class(data$Date)
 data <- data %>% 
-           mutate(Date = mdy(Date))
+           mutate(Date = ymd(Date))
 class(data$Date)
 
 # Split column Date by year, month, and day
@@ -53,7 +70,7 @@ data <- mutate(data, Year = year(Date),
 data <- data %>% 
   mutate(Old.Band.Status = Band.Status)
 
-# Replace Band.Number 'XXXXXX' with NA #### Not working.... 
+# Replace Band.Number 'XXXXXX' with NA 
 data <- data %>% 
   mutate(Band.Number = na_if(Band.Number, "XXXXXX"))
 
@@ -102,12 +119,12 @@ validation <-
   col_vals_between(vars(Green.on.back),0, 99, na_pass = TRUE) %>%
   col_vals_in_set(vars(Fat), set = c("0","1","2","3","P","T",NA)) %>%
   col_vals_in_set(vars(CP.Breed), set = c("9","8","7","5","2",NA)) %>%
-  col_vals_in_set(vars(Head.Gorget), set = c("1","2","3","F","L","M","R",NA)) %>%
-  col_vals_in_set(vars(Body), set = c("1","2","3","F","L","M","R",NA)) %>%
-  col_vals_in_set(vars(Primaries), set = c("1","2","3","4","5","6","7","8","9",
+  col_vals_in_set(vars(Head.Gorget.Molt), set = c("1","2","3","F","L","M","R",NA)) %>%
+  col_vals_in_set(vars(Body.Molt), set = c("1","2","3","F","L","M","R",NA)) %>%
+  col_vals_in_set(vars(Primaries.Molt), set = c("1","2","3","4","5","6","7","8","9",
                                            "0","F","L","M","R",NA)) %>%
-  col_vals_in_set(vars(Secondaries), set = c("1","2","3","4","5","6","F","L","M","R",NA)) %>%
-  col_vals_in_set(vars(Tail.1), set = c("1","2","3","4","5","F","L","M","R",NA)) %>% 
+  col_vals_in_set(vars(Secondaries.Molt), set = c("1","2","3","4","5","6","F","L","M","R",NA)) %>%
+  col_vals_in_set(vars(Tail.Molt), set = c("1","2","3","4","5","F","L","M","R",NA)) %>% 
   col_vals_between(vars(Weight), 2, 9, na_pass = TRUE)
   
 # Create report after validation  
@@ -127,7 +144,7 @@ which(duplicated(data$Band.Number))
 vetted_data <- data %>% 
   relocate(Protocol, CMR, Bander, Location, Date, Year, Month, Day, Time, 
            Old.Band.Status, Band.Status, Band.Number, Leg.Condition, 
-           Tarsus.Measurement, Band.Size, Species, Sex, Age, Replaced.Band.Number)
+           Tarsus.Measurement, Band.Size, Species, Sex, Age, Removed.band.num)
 
 # Create csv with vetted data. Make sure to update the site in the output name 
 write.csv(vetted_data,"output/vetted_SWRS_data.csv", row.names = FALSE)

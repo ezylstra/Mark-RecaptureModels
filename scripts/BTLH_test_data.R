2345405a794739cce9eb648002e69051bda07e33
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2022-12-06 11:01:17 -0700

    Create capture history for BTLH at ML for three years and three months

---
+++ scripts/BTLH_test_data.R
@@ -0,0 +1,187 @@
+# Preparing test-data for analysis 
+# Gaby Samaniego
+# gaby@savehummingbirds.org
+# 2022-12-01
+
+library(tidyverse)
+library(lubridate)
+library(stringr)
+library(RMark)
+library(reshape) # From internet to work with data
+
+##### DATA WRANGLING #####
+
+# Bring in raw data
+raw_data <- read.csv("data/updated_raw_data.csv")
+
+# Capitalize all characters and factors across data frame 
+# old code: band_data <- mutate_all(raw_data, .funs=toupper)  
+band_data <- raw_data %>%
+  mutate(across(!TailMid, .funs = toupper)) # To avoid error with TailMid column
+
+# Remove all leading and trailing white spaces
+# old code: band_data <- mutate_all(band_data,str_trim, side=c("both"))
+band_data <- band_data %>%
+  mutate(across(!Comment, str_trim, side = "both")) # To avoid error with comments column
+
+# Remove unnecessary columns
+band_data <- band_data %>% 
+  select(-Tarsus, -BS_Paper, -TBS_test, -TBS_Paper, -CMR, -latitude, -longitude,
+         -elevation, -region, -session, -week, -dayofyear, -DayBlock, -T1, -t3, 
+         -Diff_factor, -Diff_factorA, -TARSUS_UNIT, -BAND_UNIT, -Leg.Condition, 
+         -Orig_Tarsus.Condition, -Field_BAND.SIZE)    
+
+# Change column's names 
+band_data <- band_data %>% 
+  rename(Bander = Initials.Bdr,
+         Date = date,
+         Year = year,
+         Month = mo,
+         Day = day,
+         Time = time,
+         Old.Band.Status = OldBand.Status,
+         Tarsus.Condition = TARSUS.CONDITION,
+         Band.Size = BAND.SIZE,
+         Gorget.Color = GPORCOlOR,
+         Gorget.Count = GorCount....,
+         Head.Count = Head.GCnt....,
+         Primary.Width.P.10.Shape = PriWidth...P10.Shape,
+         Wing.Tail.Trait = Wng.TailTrait,
+         Tail.Center = TailCent,
+         Tail.Middle = TailMid,
+         Tail.Outer = TailOut,
+         Tail.Measurement = TailMeas....,
+         Wing.Chord = WingCd,
+         Pollen.Color.Location = Pollen..Color,
+         Fat = Fat.fie1d.data,
+         CP.Breed = CPBreed,
+         Head.Gorget.Molt = Gorget.head.molt,
+         Primaries.Molt = PriMary.Molt,
+         Secondaries.Molt = Secondary.Molt,
+         Day.Recaptures = Day.Recap..,
+         Recapture.Time.1 = recap.time.1,
+         Recapture.Time.2 = recap.time.2,
+         Recapture.Time.3 = recap.time.3,
+         Recapture.Time.4 = recap.time.4)
+
+# If any 'XXXXXX' in Band.Number, replace it with NA 
+if (any(unique(band_data$Band.Number) == "XXXXXX")) {
+  message("Replacing band number 'XXXXXX' with NA")
+  band_data <- band_data %>% 
+    mutate(Band.Number = na_if(Band.Number, "XXXXXX"))
+}
+
+# Format columns
+
+# Change date column from character to date
+band_data <- band_data %>% 
+  mutate(Date = mdy(Date))
+
+# Change Band.Number from character to numeric
+band_data$Band.Number <- as.numeric(as.character((band_data$Band.Number)))
+
+# Change CP.Breed from character to numeric
+band_data$CP.Breed <- as.numeric(as.character((band_data$CP.Breed)))
+
+# Merge data for banding locations that are close to each other or are moved just 
+# a little ways away but it's basically the same site
+
+# Locations to merge: 
+# MA - MA1 <- YES, Both sites close together MA1 has data  for 2002, MA has data from 2003 forward 
+# AL - AL1 <- NO, AL1 has 5 records but on different monitoring dates than AL, maybe it was a training site? Protocol is HMN for both
+# RA - RA1 <- NO, Both sites in Arizona, RA1 is a "training" site 
+# PA - PA2 <- NO, PA is a site in Arizona, PA2 is a site in British Columbia 
+# SWRS - SWRS1 <- YES, SWRS1 was the site we used while the station was closed due to covid, they are very close together
+# W1 - NO, training site 
+# T2 - NO, training site    
+
+band_data$Location[band_data$Location == "MA1"] <- "MA"
+band_data$Location[band_data$Location == "SWRS1"] <- "SWRS"
+
+##### Sort data #####
+
+# Sort data by Band.Number, Date, Species, and Sex
+band_data <- band_data %>% 
+  arrange(Band.Number, Date, Species, Sex)
+
+##### Update Band.Status ##### 
+
+# Verify that first use of a band number (date) corresponds to band status 1 (new)
+# and following captures correspond to band status R (recapture) 
+
+# Extract the rows that equal first capture (1) and recaptures (R) 
+new__recap_bands <- subset(band_data, Band.Status %in% c("1","R"))
+
+# Extract the rows that equal a band number and create a new column for Best.Band.Status
+new_bands <- unique(new__recap_bands$Band.Number)
+new__recap_bands$best_band_status <- NA
+unique(new__recap_bands$Band.Status)
+
+# Create a capture number column and add sequence of captures
+new__recap_bands$capture_number <- sequence(from = 1, rle(new__recap_bands$Band.Number)$lengths)
+
+# Fill in Best.Band.Status with 1 and R  
+new__recap_bands$best_band_status <- ifelse(new__recap_bands$capture_number == 1, "1","R")
+
+# Subset other band status from data 
+# 4 = band destroyed, 6 = band removed, 8 = band lost, F = foreign band
+other_band_status <- band_data %>% 
+  filter(Band.Status %in% c("4", "6", "8", "F"))
+
+# Merge all band status (1, R, 4, 6, 8, F)
+all_bands <- other_band_status %>% 
+  bind_rows(new__recap_bands) %>% 
+  arrange(Band.Number, Date)
+
+# Reorganize data frame, new_data Contains ALL HMN's banding data 
+new_data <- all_bands %>% 
+  select(-capture_number) %>% 
+  relocate(Protocol, Bander, State, Location, Date, Year, Month, Day, Time,
+           Old.Band.Status, Band.Status, best_band_status) %>% 
+  rename(Best.Band.Status = best_band_status)
+
+# Replace NA values for band status 4, 6, 8, and F in Best.Band.Status
+new_data$Best.Band.Status[new_data$Band.Status == "4"] <- 4
+new_data$Best.Band.Status[new_data$Band.Status == "6"] <- 6
+new_data$Best.Band.Status[new_data$Band.Status == "8"] <- 8
+new_data$Best.Band.Status[new_data$Band.Status == "F"] <- "F"
+
+
+#### check for age and sex 
+
+
+##### BTLH DATA EXPLORATION ##### 
+
+# Select BTLH data for sites that follow HMN's protocol, sex are male and female,
+# and exclude sites that have < 1 year of data and < 10 birds captured. 
+BTLH_HMN <- new_data %>% 
+  filter(Species == "BTLH", 
+         Protocol == "HMN",
+         Sex != "U", # Removes 10 individuals with unknown sex. These haven't been recaptured
+         !(Location %in% c("BL","BM","IC","IP","MXL","RR","TU","WR","AV","CNM","CO", 
+                           "MOCA","TV", "AL1","PO","AR","KS","CLAY","FL","SG","RC","SC","HSR")))
+# We know these sites have < 1 year of data and < 10 birds captured from 
+# previous data exploration 
+
+# Select one site (ML), three years (2005-2007) and data for May to July for
+# each year
+test.dat <- BTLH_HMN %>% 
+  arrange(Band.Number)%>% 
+  select(Location, Month, Band.Number, Year, Sex, Age) %>% 
+  filter(Location == "ML",
+         Year == "2005" | # Tried &, and 2005:2007 %in%
+         Year == "2006" |
+         Year == "2007",
+         Month == "5" |
+           Month == "6" |
+           Month == "7") %>%  
+  group_by(Band.Number, Year, Sex) %>%  
+  summarize(N.observation = length(Year))%>%
+  mutate(Observed = 1) %>% 
+  pivot_wider(names_from = Year, values_from = Observed, id_cols = c(Band.Number, Sex), 
+              values_fill = 0) %>% 
+  relocate(Band.Number, '2005', '2006', '2007', Sex) %>% 
+  unite(cap.his, c('2005','2006','2007'), sep = '')
+ 
+#### IT WORKS! :) 
+

c247f9ffed975350c575f45b0f2af0e1d4a7bb43
Gaby Samaniego <gabysamaniego@email.arizona.edu>
2023-07-18 07:40:53 -0700

    Update code to explore RMBL data

--- scripts/RMNP_data_exploration.R
+++ scripts/RMNP_data_exploration.R
@@ -10,16 +10,13 @@ library(stringr)
 ##### Data wrangling #####
 
 # Bring in raw data
-raw_data <- read.csv("data/RMNP_data_new.csv")
+raw_data <- read.csv("data/RMNP_data_new.csv",
+                     na.strings = c("",NA),
+                     strip.white = TRUE)
 
 # Capitalize all characters and factors across data frame 
-# old code: band_data <- mutate_all(raw_data, .funs=toupper)  
 band_data <- raw_data %>%
-  mutate(across(.funs = toupper)) 
-
-# Remove all leading and trailing white spaces
-band_data <- band_data %>% 
-  mutate_all(str_trim, side=c("both"))
+  mutate_if(is.character, str_to_upper) 
 
 # Remove unnecessary columns
 band_data <- band_data %>% 
@@ -39,8 +36,39 @@ band_data <- band_data %>%
          Age = AGE)
 
 # Remove - from Band.Number
-band_data <- band_data %>%
-  (.cols(Band.Number), str_remove_all, "-"))    
+band_data$Band.Number <- gsub("-","",as.character(band_data$Band.Number))
+# It worked but it generates an error...
 
-band_data %>% 
-  mutate(str_remove_all("[-]"))
+# Change date column from character to date
+band_data <- band_data %>% 
+  mutate(Date = mdy(Date))
+
+# Change Band.Number from character to numeric
+band_data$Band.Number <- as.numeric(as.character((band_data$Band.Number)))
+
+# Explore the data
+count(band_data, Band.Number)
+
+# Sort data by Band.Number, Date, Species, and Sex
+band_data <- band_data %>% 
+  arrange(Band.Number, Date, Species, Sex)
+
+# Data exploration
+unique(band_data$Bander)       # Banders Fred and Tina Engelmann
+unique(band_data$Location)     # All records are from RMBL
+unique(band_data$Species)      # All records are BTLH
+unique(band_data$Band.Status)  # Records are either 1 or R
+unique(band_data$Sex)          # Records are for M and F
+unique(band_data$Age)          # All records are for age 1
+
+# Summarize data
+RMBL_data <- band_data %>% 
+  summarize(N.Captures = length(Band.Number),
+            Individuals.Banded = length(unique(Band.Number)),
+            N.Recaptures = length(unique(Band.Number[Band.Status == "R"])),
+            N.Males = length(unique(Band.Number[Sex == "M"])),
+            N.Females = length(unique(Band.Number[Sex == "F"]))) %>% 
+  as.data.frame
+
+
+

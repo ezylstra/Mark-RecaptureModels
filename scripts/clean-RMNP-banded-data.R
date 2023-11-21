b5ba77e163662db70fbb257a3b6018d798242ca5
Gaby Samaniego <gabysamaniego@arizona.edu>
2023-11-21 10:19:48 -0700

    Edit code to fill in empty columns

--- scripts/clean-RMNP-banded-data.R
+++ scripts/clean-RMNP-banded-data.R
@@ -17,7 +17,7 @@ banded.files <- list.files(path = "data/RMNP-banded-by-year/",
 banded.dat <- lapply(banded.files, fread, sep = ",")  
 
 # Combine all csv files in a data frame
-banded.dat <- rbindlist(banded.dat)
+banded.dat <- rbindlist(banded.dat, fill = TRUE)
 
 # Replace space in column names with '_'
 banded.dat <- banded.dat %>% 
@@ -41,13 +41,15 @@ banded.dat <- mutate(banded.dat, year = year(date),
 # Fix bander... later. Use the same code for Fred and Tena
 
 # Change any SY (second year) in age to AHY (after hatch-year)
+unique(banded.dat$age)
 banded.dat$age[banded.dat$age == 'SY'] <- 'AHY'
 
 # Fix other columns later... maybe
 
 # Summarize data for analysis 
 dat <- banded.dat %>% 
-  select(band_number, date, site, species, sex, age, year, day, month) %>% 
+  select(band_number, date, site, species, sex, age, year, day, month) %>%
+  mutate(band_status = 1) %>% # 1 for all new bands applied
   filter(species == 'BTLH')
 
 # Check for duplicated band numbers

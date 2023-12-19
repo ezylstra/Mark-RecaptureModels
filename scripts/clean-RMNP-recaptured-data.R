d0fccc2e5ad25d6777001c187b11e33a31afeb9e
Gaby Samaniego <gabysamaniego@arizona.edu>
2023-12-19 06:13:16 -0700

    Delete one more record

--- scripts/clean-RMNP-recaptured-data.R
+++ scripts/clean-RMNP-recaptured-data.R
@@ -252,9 +252,10 @@ recaptured.dat <- recaptured.dat %>%
          band_number != '9000-93749', # This record is not in banded data
          band_number != '9000-93765') # This record is not in banded data
 
-# Remove this record because I removed it from banded data
+# Remove these records because I removed them from banded data
 recaptured.dat <- recaptured.dat %>% 
-  filter(band_number != '5000-96919')
+  filter(band_number != '5000-96919',
+         band_number != '5000-29395')
 
 # Lines 234 to 257 remove 19 records from data.
 

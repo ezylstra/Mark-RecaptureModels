8ffa1f928d49e4fdf06f3ab5ad07950f2fdc5404
Erin Zylstra <erinzylstra@gmail.com>
2024-02-21 13:59:54 -0700

    Finish extracting site info from recapture data

--- scripts/extract-recapture-info.R
+++ scripts/extract-recapture-info.R
@@ -3,8 +3,9 @@
 
 # Load packages
 library(tidyverse)
-library(data.table)
-library(janitor)
+# library(data.table)
+# library(janitor)
+library(terra)
 
 rm(list = ls())
 
@@ -29,6 +30,8 @@ rm(list = ls())
 # Load cleaned, full recap file:
 recap <- read.csv("output/cleaned-recaptured-data-RMNP-full.csv",
                   na.strings = c(NA, ""))
+colnames(recap)[colnames(recap) %in% paste0("r", 1:17)] <- 
+  paste0("r", str_pad(1:17, width = 2, pad = "0"))
 
 # Load cleaned banded file:
 banded <- read.csv("output/cleaned-banded-data-RMNP.csv") %>%
@@ -43,26 +46,32 @@ sites <- sort(unique(sites_df$site))
 # Load letter codes for reference
 letters <- read.csv("data/BBL_letter_codes.csv")
 
+# Before I do anything else, need to fix or remove problematic band numbers
+# from recap dataset (see merge-cleaned-banded-recapture-data-RMNP.R)
+band_remove <- read.csv("data/recap-band-remove.csv")
+band_fix <- read.csv("data/recap-band-fix.csv")
 
-# PICK UP HERE: need to use code form merge-cleaned-....R so that all the 
-# band numbers match up in both datasets....
-
-
-
+recap <- recap %>%
+  filter(!band %in% band_remove$band_to_remove) %>%
+  left_join(band_fix, join_by("band" == "old")) %>%
+  mutate(band = if_else(is.na(new), band, new)) %>%
+  select(-new)
+# Check
+# recap$band[!recap$band %in% banded$band]
 
 # Is there an entry in the first recapture column for every bird/yr?  
-sum(is.na(recap$r1)) # No, 7 NAs
-filter(recap, is.na(r1))
+sum(is.na(recap$r01)) # No, 7 NAs
+filter(recap, is.na(r01))
 # There are 7 rows with no entry in recapture columns
 # (foreign, rebands, found dead)
   # 2 dead, will remove
   # 4 with NEW BAND and former number in comments, but this recapture info is in
   # banded, so can remove here too.
   # One with no comments (9000-12585). Maybe didn't enter info, but ok?
-recap <- filter(recap, !(is.na(r1) & !is.na(comments)))
+recap <- filter(recap, !(is.na(r01) & !is.na(comments)))
 # Going to enter fake date into r1 column for remaining row (9000-12585 in 2010) 
 # so that the recap info doesn't disappear if I convert to long form
-recap$r1[is.na(recap$r1)] <- "7/15"
+recap$r01[is.na(recap$r01)] <- "7/15"
 
 # Move information about rebanding events from banded to recap ----------------#
 
@@ -91,21 +100,20 @@ rebands_to_recap <- rebands %>%
   select(-c(reband_age, band_yr, band_age, band_sex)) %>%
   rename(recap_yr = reband_yr,
          sex = reband_sex,  # checked that sex is always same (orig, reband)
-         r1 = reband_site,
+         r01 = reband_site,
          site_orig = band_site) %>%
-  relocate(site_orig, .before = r1) %>%
-  mutate(r2 = NA, r3 = NA, r4 = NA, r5 = NA, r6 = NA, r7 = NA, r8 = NA, r9 = NA,
-         r10 = NA, r11 = NA, r12 = NA, r13 = NA, r14 = NA, r15 = NA, r16 = NA,
-         r17 = NA, comments = NA)
+  relocate(site_orig, .before = r01) %>%
+  mutate(r02 = NA, r03 = NA, r04 = NA, r05 = NA, r06 = NA, r07 = NA, r08 = NA, 
+         r09 = NA, r10 = NA, r11 = NA, r12 = NA, r13 = NA, r14 = NA, r15 = NA, 
+         r16 = NA, r17 = NA, comments = NA)
 
 recap <- rbind(recap, rebands_to_recap)
 
 # Convert to long form --------------------------------------------------------#
 
-
 recapl <- recap %>%
-  pivot_longer(cols = r1:r17, 
-               names_to = "r_num",
+  pivot_longer(cols = r01:r17, 
+               names_to = "recap_num",
                values_to = "entry") %>%
   data.frame() %>%
   filter(!is.na(entry)) %>%
@@ -126,6 +134,22 @@ filter(recapl, sex != band_sex)
 # Will likely want to rely sex assigned at least capture, but can deal with this
 # later
 
+# Explore sites ---------------------------------------------------------------#
+
+# Load DEM
+dem <- rast("data/dem.tif")
+
+plot(dem)
+points(latitude ~ longitude, data = sites_df, pch = 19, cex = 0.7)
+
+plot(dem, xlim = c(-105.9, -105.4), ylim = c(40.1, 40.55))
+points(latitude ~ longitude, data = sites_df, pch = 19, cex = 0.7)
+text(latitude ~ longitude, labels = site, data = sites_df, pos = 3)
+
+# Adding East/West delineation to sites (based on DEM)
+west <- "WPK1|CLP|SHIP|HOLZ|GNMTN|BGMD|KV1|POLC"
+sites_df$ew <- ifelse(str_detect(sites_df$site, west), "west", "east")
+
 # Sites -----------------------------------------------------------------------#
 
 sites_search <- paste0(c(sites, tolower(sites)), collapse = "|")
@@ -135,41 +159,177 @@ recapl <- recapl %>%
 
 filter(recapl, at == 1 & is.na(site))
 # Other locs? YMCA (n = 6); HPK3 (6); Peaceful Valley, CO (1); ARIZONA (1)
-# YMCA is on/near park boundary, so may be close to other sites
-# Will remove Peaceful Valley and ARIZONA for now since they're either outside 
-# park or unknown
+# According to Google Maps, YMCA is close to MP1/HPK2/HPK1 complex. 
+# Assuming HPK3 near other HPKs. 
+# Will remove Peaceful Valley and ARIZONA since they're outside park or unknown
 
 # Adding a few sites (that may or may not have been regular sampling locations)
-sites_search <- paste0(sites_search, "YMCA", "HPK3")
+sites_search <- paste0(c(sites_search, "YMCA", "HPK3"), collapse = "|")
+sites_to_add <- data.frame(site = c("YMCA", "HPK3"),
+                           latitude = NA,
+                           longitude = NA,
+                           elevation = NA,
+                           ew = "east")
+sites_df <- rbind(sites_df, sites_to_add)
 
+# Remove recaps at other sites, create recap_site column and indicator if
+# recap is in banding year (first_yr)
 recapl <- recapl %>%
   filter(!str_detect(entry, "Peaceful Valley|ARIZONA")) %>%
   mutate(site = toupper(str_extract(entry, pattern = sites_search))) %>%
-  select(-at) %>%
   mutate(recap_site = if_else(is.na(site), band_site, site)) %>%
-  mutate(first_yr = if_else(recap_yr == band_yr, 1, 0)) 
+  mutate(first_yr = if_else(recap_yr == band_yr, 1, 0)) %>%
+  select(-c(at, site))
 
+# Create data set with band number, sites captured at
+band_site <- band_first %>%
+  select(band, band_yr, band_site) %>%
+  rename(yr = band_yr,
+         site = band_site) %>%
+  mutate(band_status = "1") %>%
+  mutate(recap_num = "r00")
 
+recap_site <- recapl %>%
+  select(band, recap_yr, recap_site, band_status, recap_num) %>%
+  rename(yr = recap_yr,
+         site = recap_site)
 
-# See how often these movements occurred within/between seasons
-recapl <- recapl %>%
+# Combine banding and recap info, removing duplicates (and R if in the first
+# year at the same site)
+cap_sites <- rbind(band_site, recap_site) %>%
+  distinct() %>%
+  arrange(band, band_status, yr, recap_num, site) %>%
+  distinct(band, yr, site, .keep_all = TRUE)
+
+# Movements between sites
+  # For each bird, calculate the number of unique sites captured at (n_sites) as
+  # well as the number of movements (n_moves)
+  bird_sites <- cap_sites %>%
+    group_by(band) %>%
+    summarize(band_site = site[1],
+              band_yr = yr[1],
+              n_sites = length(unique(site)),
+              n_moves = length(rle(site)$lengths) - 1,
+              site2 = ifelse(n_moves > 0, rle(site)$values[2], NA),
+              site3 = ifelse(n_moves > 1, rle(site)$values[3], NA),
+              site4 = ifelse(n_moves > 2, rle(site)$values[4], NA),
+              site5 = ifelse(n_moves > 3, rle(site)$values[5], NA),
+              .groups = "keep") %>%
+    data.frame()
+  count(bird_sites, n_moves) # 355 birds moved at least once
+  count(bird_sites, n_sites) # 341 birds used 2 sites, 14 used 3 sites
+
+  # Create data set of movements
+  movements <- cap_sites %>%
+    filter(band %in% bird_sites$band[bird_sites$n_moves > 0]) %>%
+    arrange(band, band_status, yr, recap_num) %>%
+    mutate(site_previous = NA,
+           time = NA)
   
+  for (i in 2:nrow(movements)) {
+    if (movements$band[i] == movements$band[i - 1]) {
+      movements$site_previous[i] <- movements$site[i - 1]
+      movements$time[i] <- movements$yr[i] - movements$yr[i - 1]
+    } else {
+      movements$site_previous[i] <- NA
+      movements$time[i] <- NA
+    }
+  }
+  
+  movements <- movements %>%
+    filter(site != site_previous) %>%
+    mutate(same_yr = if_else(time > 0, 0, 1)) %>%
+    select(-c(yr, band_status, time))
+  
+  nrow(movements)                # 447 movements
+  length(unique(movements$band)) # 355 birds
+  count(movements, same_yr)      # 205 movements within year, 242 across years
+  
+  # Get distances between sites
+  locs <- vect(sites_df, geom = c("longitude", "latitude"), crs = "EPSG:4326")
+  sites_df$site_num <- 1:nrow(sites_df)
+  dists <- distance(locs, unit = "km", pairs = TRUE, symmetrical = FALSE)
+  dists <- as.data.frame(dists)
+  dists$from_name <- sites_df$site[match(dists$from, sites_df$site_num)]
+  dists$to_name <- sites_df$site[match(dists$to, sites_df$site_num)]
+  dists <- dists %>%
+    select(-c(from, to)) %>%
+    rename(dist = value,
+           from = from_name,
+           to = to_name)
 
+  movements <- left_join(movements, dists, 
+                         join_by("site_previous" == "from", "site" == "to")) 
+  movements$site_ew <- sites_df$ew[match(movements$site, sites_df$site)]
+  movements$site_previous_ew <- sites_df$ew[match(movements$site_previous, sites_df$site)]
+  movements <- movements %>%
+    mutate(over_ridge = if_else(site_ew != site_previous_ew, 1, 0)) %>%
+    select(-c(site_ew, site_previous_ew))
 
-# Look at individuals that moved between sites
-moved <- recapl %>%
-  filter(band_site != recap_site)
+  filter(movements, over_ridge == 1)
+  filter(banded, band == movements$band[movements$over_ridge == 1])
+  filter(recap, band == movements$band[movements$over_ridge == 1])
+  # Just a single bird was recaptured on both sides of the ridge
+  # Banded on west site (HOLZ) in 2008, then captured on east site (MCGC) in 
+  # May 2009 and then back at HOLZ in July 2009.
 
-length(unique(moved$band)) # 343 birds 
+  # Summary of all movements (for which distances are known, so excludes YMCA 
+  # and HPK3, but only movements to/from these sites are to others nearby [MP1,
+  # HPK1, HPK2])
+  filter(movements, site %in% c("YMCA", "HPK3") | site_previous %in% c("YMCA", "HPK3"))
+  
+  move_all <- movements %>% 
+    filter(!is.na(dist))
+  (n_dists_small <-  sum(move_all$dist < 2.5))
+  n_dists_small / nrow(move_all)
+    # 224 movements (52% of known movements distances) < 2.5 km
+  (n_dists_big <-  sum(move_all$dist > 10))
+  n_dists_big / nrow(move_all)
+    # 157 movements (36% of known movements distances) > 10 km
+  
+  # Just looking at inter-year movements
+  move_iy <- movements %>%
+    filter(!is.na(dist)) %>%
+    filter(same_yr == 0)
+  (n_dists_iy_small <- sum(move_iy$dist < 2.5)) 
+  n_dists_iy_small / nrow(move_iy)
+  # 139 movements (60% of known movement distances) < 2.5 km
+  (n_dists_iy_big <- sum(move_iy$dist > 10)) 
+  n_dists_iy_big / nrow(move_iy)
+  # 67 movements (29% of known movement distances) > 10 km
+  
+  # How many times did birds get recaptured in different years?
+  recap_events <- cap_sites %>%
+    select(band, yr, band_status) %>%
+    distinct(band, yr, .keep_all = TRUE)
+  n_recap_events <- sum(recap_events$band_status == "R") # 2465 recapture events
+  
+  n_dists_iy_big / n_recap_events 
+  # 3% of recapture events did not occur at site of previous capture
 
+  # Common movements (regarless of direction)
+  for (i in 1:nrow(movements)) {
+    sites_row <- sort(c(movements$site[i], movements$site_previous[i]))
+    movements$sitea[i] <- sites_row[1]
+    movements$siteb[i] <- sites_row[2]
+  }
+  common_moves <- count(movements, sitea, siteb, dist) %>%
+    arrange(desc(n), sitea)
+  common_moves
+  # Most common move is between CC3 and MCGC, then
+  # HPK1 AND MP1 (same cluster)
+  # CC2 and MCGC (same cluster)
+  # HPK1 and HPK2 (same cluster)
+  # HPK2 and MP1 (same cluster)
+  # GNMTN and HOLZ
+  # all other pairs occurred < 22 times
 
-# Create dataframe with site pairs
-  # Number of "movements"
-  # Distance between sites
-  # Same site "cluster"?  (need to add this information to sites...)
+  # Questioning whether the location for CC3 is correct given 
+  # 1) its far from CC2 (almost 18 km), and 
+  # 2) how many movements between CC3 and MCGC given 18.3 km distance.
 
-
-
+# Lack of movements between east and west side suggests that maybe it's worth 
+# separating the two areas.
 
 # Dates -----------------------------------------------------------------------#
 
@@ -189,15 +349,16 @@ dates <- paste0(c(mmdd, mdd, mmd, md, ddMON, dMON, m.dd, m.d, mdd2, md2),
                 collapse = "|")
 
 # Extract date (in any format) from r1 column
-datesd <- str_extract(recap$r1, dates)
-sum(is.na(datesd)) # 2 entries where a date wasn't extracted
-recap[is.na(datesd), c("band", "recap_yr", "r1", "comments")] # Yup, no date
+datesd <- str_extract(recap$r01, dates)
+sum(is.na(datesd)) 
+# 66 entries where a date wasn't extracted (almost all are those I moved from
+# banded dataset)
 
 sort(unique(datesd)) # Most look ok (but need to check 4/16, 61/7, 7/63, 9/22)
 
 # Check all other recapture columns
-r217 <- as.matrix(recap[,paste0("r", 2:17)])
-# Create a vector (reads down r2 column, then r3 column, etc)
+r217 <- as.matrix(recap[,paste0("r", str_pad(2:17, width = 2, pad = "0"))])
+# Create a vector (reads down r02 column, then r03 column, etc)
 recap217 <- as.vector(ifelse(is.na(r217), 0, 1))
 # Extract dates
 dates217 <- str_extract(r217, dates)
@@ -207,13 +368,3 @@ recap217_check <- which(recap217 == 1 & is.na(dates217))
 # Find rows with missing date entries
 rows_missingdates <- arrayInd(recap217_check, .dim = dim(r217))[,1]
 recap[rows_missingdates,]
-
-
-
-
-
-
-
-
-
-
